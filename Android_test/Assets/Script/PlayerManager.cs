c2db02e9c0e372a5a7cfb368ab7e12930733458c
31good <45097407+31good@users.noreply.github.com>
2021-12-15 16:11:26 -0500

    Revert "Revert "room clear logic added""
    This reverts commit a2d9ab4bbd4dc2f281d50993d0ea8cf1bb271b3b.

---
+++ Android_test/Assets/Script/PlayerManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private static PlayerManager Instance;
+    public static PlayerManager GetInstance()
+    {
+        return Instance;
+    }
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public GameObject room001;
+    public GameObject room002;
+
+    public int roomIndex = 1;//玩家所在的房间号
+    public List<SpriteRenderer> enemys001 = new List<SpriteRenderer>();
+    public List<SpriteRenderer> enemys002 = new List<SpriteRenderer>();
+
+    void Start()
+    {
+        for (int i = 0; i < room001.GetComponentsInChildren<SpriteRenderer>().Length; i++)
+        {
+            enemys001.Add(room001.GetComponentsInChildren<SpriteRenderer>()[i]);
+        }
+        for (int i = 0; i < room002.GetComponentsInChildren<SpriteRenderer>().Length; i++)
+        {
+            enemys002.Add(room001.GetComponentsInChildren<SpriteRenderer>()[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            foreach (var item in enemys001)
+            {
+                item.GetComponent<meleeenemy>().taken_damage(5);
+            }
+        }
+    }
+}
