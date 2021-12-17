e6eb768a01cba867f5004704474367a33ff1f67d
FantasiA10 <yw4177@nyu.edu>
2021-12-16 22:46:08 -0500

    update

--- Android_test/Assets/Script/shop.cs
+++ Android_test/Assets/Script/shop.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class shop : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject item;
+    public GameObject introduction;
+    public GameObject player;
+    public string type = null;
+    private int random_num;
     void Start()
     {
-        
+        random_num = Random.Range(0,14);
+        int count = 0;
+        foreach(Transform child in this.transform){
+            if(count == random_num){
+                child.gameObject.SetActive(true);
+                item = child.gameObject;
+            }
+            else{
+                child.gameObject.SetActive(false);
+            }
+            count +=1;
+        }
+        foreach(Transform child in this.transform){
+            if(child.tag == "introduction"){
+                introduction = child.gameObject;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "player"){
+            introduction.SetActive(true);
+        } 
+    }
+    private void OnTriggerExit2D(Collider2D other) {
+        if(other.tag == "player"){
+            introduction.SetActive(false);
+        }
     }
 }
